0fa9b82774a5ab30dd7c28a8b482d65eac7aecfd
mennooo <menno.hoogendijk@iadvise.eu>
2018-03-07 17:21:32 +0100

    no message

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,5 +1,6 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 
 var User = require('../models/User');
@@ -15,8 +16,6 @@ passport.deserializeUser(function(id, done) {
 
 // Sign in with Email and Password
 passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
-  // Save username and password for REST
-  console.log(email, password)
   new User({ email: email })
     .fetch()
     .then(function(user) {
@@ -31,4 +30,62 @@ passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, passw
         return done(null, user);
       });
     });
+}));
+
+// Sign in with Facebook
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_ID,
+  clientSecret: process.env.FACEBOOK_SECRET,
+  callbackURL: '/auth/facebook/callback',
+  profileFields: ['name', 'email', 'gender', 'location'],
+  passReqToCallback: true
+}, function(req, accessToken, refreshToken, profile, done) {
+  if (req.user) {
+    new User({ facebook: profile.id })
+      .fetch()
+      .then(function(user) {
+        if (user) {
+          req.flash('error', { msg: 'There is already an existing account linked with Facebook that belongs to you.' });
+          return done(null);
+        }
+        new User({ id: req.user.id })
+          .fetch()
+          .then(function(user) {
+            user.set('name', user.get('name') || profile.name.givenName + ' ' + profile.name.familyName);
+            user.set('gender', user.get('gender') || profile._json.gender);
+            user.set('picture', user.get('picture') || 'https://graph.facebook.com/' + profile.id + '/picture?type=large');
+            user.set('facebook', profile.id);
+            user.save(user.changed, { patch: true }).then(function() {
+              req.flash('success', { msg: 'Your Facebook account has been linked.' });
+              done(null, user);
+            });
+          });
+      });
+  } else {
+    new User({ facebook: profile.id })
+      .fetch()
+      .then(function(user) {
+        if (user) {
+          return done(null, user);
+        }
+        new User({ email: profile._json.email })
+          .fetch()
+          .then(function(user) {
+            if (user) {
+              req.flash('error', { msg: user.get('email') + ' is already associated with another account.' });
+              return done();
+            }
+            user = new User();
+            user.set('name', profile.name.givenName + ' ' + profile.name.familyName);
+            user.set('email', profile._json.email);
+            user.set('gender', profile._json.gender);
+            user.set('location', profile._json.location && profile._json.location.name);
+            user.set('picture', 'https://graph.facebook.com/' + profile.id + '/picture?type=large');
+            user.set('facebook', profile.id);
+            user.save().then(function(user) {
+              done(null, user);
+            });
+          });
+      });
+  }
 }));
--- app/models/Products.js
+++ app/models/Products.js
@@ -1,3 +1,4 @@
+
 var rest = require('../config/rest')({
   secured: false
 })
